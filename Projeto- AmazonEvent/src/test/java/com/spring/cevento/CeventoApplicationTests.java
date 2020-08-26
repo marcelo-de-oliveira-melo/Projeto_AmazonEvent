7151da0eca28366276b2ce9631778cc15be59229
marcelo-de-oliveira-melo <60896093+marcelo-de-oliveira-melo@users.noreply.github.com>
2020-08-26 05:07:21 -0400

    Add files via upload

---
+++ Projeto- AmazonEvent/src/test/java/com/spring/cevento/CeventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cevento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
